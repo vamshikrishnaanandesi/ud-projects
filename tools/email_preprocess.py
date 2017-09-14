1ff4a22cca1276f95a0c93fcedc018e1066711a5
vamshikrishnaanandesi <vamshikrishnaanandesi@gmail.com>
2017-09-14 13:19:46 +0530

    arial_final

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -61,5 +61,4 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     ### info on the data
     print "no. of Chris training emails:", sum(labels_train)
     print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
-    
-return features_train_transformed, features_test_transformed, labels_train, labels_test
+    return features_train_transformed, features_test_transformed, labels_train, labels_test
